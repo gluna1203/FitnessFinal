f87e1481133e20a76e957ef86dc921225a3525af
GabrielL <79658132+gluna1203@users.noreply.github.com>
2023-05-29 22:59:07 -0600

    ...

--- FitnessApp/Pages/WorkoutList.php
+++ FitnessApp/Pages/WorkoutList.php
@@ -1,7 +1,7 @@
 <?php
 include_once "Header.php";
 include_once($_SERVER['DOCUMENT_ROOT'].'/dbConnection/UserConnection.php');
-include_once($_SERVER['DOCUMENT_ROOT'].'/dbConnection/MuscleConnection.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/dbConnection/crud.php');
 include_once($_SERVER['DOCUMENT_ROOT']."/Utility.php");
 
 //if (!logged()) {
