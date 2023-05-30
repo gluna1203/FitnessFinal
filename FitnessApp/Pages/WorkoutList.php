2bce1b948e2c45170403a1e12b63f0b1a27a9fa8
GabrielL <79658132+gluna1203@users.noreply.github.com>
2023-05-29 21:46:38 -0600

    Save array to text file added

---
+++ FitnessApp/Pages/WorkoutList.php
@@ -0,0 +1,12 @@
+<?php
+include_once "Header.php";
+include_once($_SERVER['DOCUMENT_ROOT'].'/dbConnection/UserConnection.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/dbConnection/MuscleConnection.php');
+include_once "Utility.php";
+?>
+
+
+
+<?php
+include_once "Footer.php"
+?>
---
+++ FitnessApp/Utility.php
@@ -0,0 +1,8 @@
+<?php
+function saveToText($array){
+    $json = json_encode($array);
+    $file = fopen('workoutlist.txt', 'w');
+    fwrite($file, $json);
+    fclose($file);
+}
+?>
