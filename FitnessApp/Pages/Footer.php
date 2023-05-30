2f5fb8582fb1f908d501f5ca838621a41e1d114b
GabrielL <79658132+gluna1203@users.noreply.github.com>
2023-05-29 21:11:09 -0600

    Header footer

--- FitnessApp/Pages/Footer.php
+++ FitnessApp/Pages/Footer.php
@@ -1,3 +1,5 @@
-<?php
-
-?>
+<!-- Basic HMTL footer with copyright-->
+<br />
+<p>&copy;2023 Team Neapolitan</p>
+</body>
+</html>
--- FitnessApp/Pages/Header.php
+++ FitnessApp/Pages/Header.php
@@ -1,3 +1,11 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en-ca" class="background>
+
+<head>
+    <meta content="text/html"; charset="iso-8859-1" http-equiv="content-type"/>
+    <link rel="stylesheet" href="/style.php">   
+</head>
+<body>
