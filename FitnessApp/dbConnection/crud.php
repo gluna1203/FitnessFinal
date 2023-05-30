1748c45ef55c23af8bc9d8bab666bf2f67bfe436
qhough <qhough@student.neumont.edu>
2023-05-29 22:06:16 -0600

    made the crud page

---
+++ FitnessApp/dbConnection/crud.php
@@ -0,0 +1,155 @@
+<?php
+
+define("DB_USER", "root");
+define("DB_PSWD", "Neumont#23"); // Add password
+define("DB_SERVER", "localhost"); // Value might change depending on the MySQL installation
+define("DB_NAME", "muscledb"); // Might be different on your PC
+
+function ConnGetB() {
+    $dbConn = mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME)
+        or die("Failed to connect to the database: " . mysqli_connect_error());
+    return $dbConn;
+}
+
+if (isset($_POST['create'])) {
+    $conId = $_POST['conId'];
+    $name = $_POST['name'];
+    $manufacturer = $_POST['manufacturer'];
+    $unitsSold = $_POST['unitsSold'];
+    $releaseYear = $_POST['releaseYear'];
+
+    echo "<script>alert('conId: $conId, name: $name, manufacturer: $manufacturer, unitsSold: $unitsSold, releaseYear: $releaseYear');</script>";
+
+    create($conId, $name, $manufacturer, $unitsSold, $releaseYear);
+}
+
+/*
+if (isset($_POST['update'])) {
+    $conId = $_POST['conId'];
+    $name = $_POST['name'];
+    $manufacturer = $_POST['manufacturer'];
+    $unitsSold = $_POST['unitsSold'];
+    $releaseYear = $_POST['releaseYear'];
+
+    echo "<script>alert('conId: $conId, name: $name, manufacturer: $manufacturer, unitsSold: $unitsSold, releaseYear: $releaseYear');</script>";
+
+    update($conId, $name, $manufacturer, $unitsSold, $releaseYear);
+}
+
+if (isset($_POST['delete'])) {
+    $val = $_POST['conId'];
+    deleteOne($val);
+}
+
+*/
+if (isset($_POST['readById'])) {
+    $val = $_POST['conId'];
+    $record = getRecordById($val);
+    if ($record) {
+        $conId = $record['conId'];
+        $name = $record['name'];
+        $manufacturer = $record['manufacturer'];
+        $unitsSold = $record['unitsSold'];
+        $releaseYear = $record['releaseYear'];
+        $alertMessage = "conId: $conId, name: $name, manufacturer: $manufacturer, unitsSold: $unitsSold, releaseYear: $releaseYear";
+        echo "<p style='text-align: center;'>$alertMessage</p>";
+    } else {
+        echo "<script>alert('Record not found');</script>";
+    }
+}
+
+if (isset($_POST['readAll'])) {
+    $sql = "SELECT * FROM consoles.consoles";
+    $result = ConnGetB()->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $conId = $row['conId'];
+            $name = $row['name'];
+            $manufacturer = $row['manufacturer'];
+            $unitsSold = $row['unitsSold'];
+            $releaseYear = $row['releaseYear'];
+            $alertMessage = "conId: $conId, name: $name, manufacturer: $manufacturer, unitsSold: $unitsSold, releaseYear: $releaseYear";
+            echo "<p style='text-align: center;'>$alertMessage</p><br>";
+        }
+    }else {
+        echo "<script>alert('Records not found');</script>";
+    }
+}
+
+/*
+// Create
+function create($workout, $name) {
+    $dbConn = ConnGetB();
+
+    $sql = "INSERT INTO muscledb.workouts (workout, name)
+            VALUES ('$workout', '$name')";
+
+    if ($dbConn->query($sql) === TRUE) {
+        echo "Record created successfully";
+    } else {
+        echo "Error creating record: " . $dbConn->error;
+    }
+
+    $dbConn->close();
+}
+*/
+
+// Read
+function readAll() {
+    $dbConn = ConnGetB();
+    $sql = "SELECT * FROM muscledb.muscles";
+    $result = $dbConn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_array()) {
+            // Display the array or perform any necessary operations
+            echo "Muscles ID: " . $row['musclesId'] .,  "Muscles ID: " . $row['musclesId'] . " ,Name: " . $row['name'] . ", Manufacturer: " . $row['manufacturer'] . ", Units Sold: " . $row['unitsSold'] . ", Release Year: " . $row['releaseYear'] . "<br>";
+        }
+    } else {
+        echo "No records found";
+    }
+
+    $dbConn->close();
+}
+/*
+
+
+//read by ID
+function getRecordById($conId) {
+    $sql = "SELECT * FROM consoles.consoles WHERE conId = '$conId'";
+    $result = ConnGetB()->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        return $row;
+    } else {
+        return null;
+    }
+}
+
+
+// Update
+function update($conId, $name, $manufacturer, $unitsSold, $releaseYear) {
+    $dbConn = ConnGetB();
+
+    $sql = "UPDATE consoles.consoles SET name='$name', manufacturer='$manufacturer', unitsSold='$unitsSold', releaseYear='$releaseYear' WHERE conId='$conId'";
+
+    if ($dbConn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $dbConn->error;
+    }
+
+    $dbConn->close();
+}
+
+
+// Delete
+function deleteOne($conId) {
+    $dbConn = ConnGetB();
+    $sql = "DELETE FROM consoles.consoles WHERE conId='$conId'";
+    $dbConn->query($sql) === TRUE || die("Error deleting record: " . $dbConn->error);
+    $dbConn->close();
+}
+
+
+*/
+?>
