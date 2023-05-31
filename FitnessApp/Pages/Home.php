481c4686d4c2632b470c2d69d6b79ff2eb40ad85
Jesse3evans <78041192+Jesse3evans@users.noreply.github.com>
2023-05-30 22:26:43 -0600

    home page rework

--- FitnessApp/Pages/Home.php
+++ FitnessApp/Pages/Home.php
@@ -1,13 +1,12 @@
-
 <?php
 include_once "Header.php";
 include_once($_SERVER['DOCUMENT_ROOT']."/dbConnection/UserConnection.php");
 include_once($_SERVER['DOCUMENT_ROOT']."/dbConnection/MuscleConnection.php");
 include_once($_SERVER['DOCUMENT_ROOT']."/Utility.php");
 
-if (!logged()) {
-    header('location: /Pages/LoginPage.php');
-}
+//if (!logged()) {
+//    header('location: /Pages/LoginPage.php');
+//}
 
 ?>
 
@@ -21,49 +20,6 @@ if (!logged()) {
     <button name="areaId" value="5">Legs</button>
     <button name="areaId" value="6">Arms</button>
 </form>
-
-
-<script>
-$(document).ready(function() {
-  $('#muscleForm').submit(function(event) {
-    event.preventDefault();
-
-    // Get the selected areaId
-      var areaId = $(this).find('button[name="areaId"]:focus').val();
-    $('#muscleTable tbody').empty()
-
-    // Make an AJAX request to retrieve the muscle information
-    $.ajax({
-      url: '/dbConnection/MuscleConnection.php',
-      type: 'POST',
-      data: { areaId: areaId },
-      dataType: 'json',
-      success: function(response) {
-        // Display the muscle information in an alert
-        var message = '';
-
-          for (var i = 0; i < response.length; i++) {
-          var muscle = response[i];
-
-
-          var tr_str = "<tr>" +
-          "<td align='center'>" + muscle.muscleId + "</td>" +
-          "<td align='center'>" + muscle.name + "</td>" +
-          "<td align='center'>" + muscle.workout1+ "</td>" +
-              "<td align='center'>" + muscle.workout2 + "</td>" +
-              '<td align="center"><a href='+ muscle.videoLink +' target="_blank">Video</a></td>' +
-                  "</tr>";
-              $("#muscleTable tbody").append(tr_str);
-          }
-      },
-      error: function(xhr, status, error) {
-        console.log(error);
-        alert('An error occurred while retrieving muscle information.');
-      }
-    });
-  });
-});
-</script>
 
 <div class="container">
     <table id="muscleTable" border="1">
@@ -80,11 +36,117 @@ $(document).ready(function() {
     </table>
 </div>
 
+<div>
+    <label for="muscleIdInput">Muscle ID:</label>
+    <input type="text" id="muscleIdInput" />
+    <button id="addToWorkoutBtn">Add to Workout</button>
+</div>
+
+<h2>Workout List</h2>
+<ul id="muscleList"></ul>
+
+<button id="addWorkoutBtn">Add Workout</button>
+
 <form method="post" action="Home.php">
     <div class="user-values">
         <button type="submit" class="btn" name="logout">Logout</button>
     </div>
 </form>
+
+<script>
+$(document).ready(function() {
+  var muscleList = [];
+
+  function updateMuscleList() {
+    var listHtml = '';
+
+    for (var i = 0; i < muscleList.length; i++) {
+      var muscleId = muscleList[i];
+
+      listHtml += '<li>' + muscleId + '</li>';
+    }
+
+    $('#muscleList').html(listHtml);
+  }
+
+  $('#muscleForm').submit(function(event) {
+    event.preventDefault();
+
+    var areaId = $(this).find('button[name="areaId"]:focus').val();
+    $('#muscleTable tbody').empty();
+
+    $.ajax({
+      url: '/dbConnection/MuscleConnection.php',
+      type: 'POST',
+      data: { areaId: areaId },
+      dataType: 'json',
+      success: function(response) {
+        var message = '';
+
+        for (var i = 0; i < response.length; i++) {
+          var muscle = response[i];
+
+          var tr_str = "<tr>" +
+            "<td align='center'>" + muscle.muscleId + "</td>" +
+            "<td align='center'>" + muscle.name + "</td>" +
+            "<td align='center'>" + muscle.workout1 + "</td>" +
+            "<td align='center'>" + muscle.workout2 + "</td>" +
+            '<td align="center"><a href="' + muscle.videoLink + '" target="_blank">Video</a></td>' +
+            "</tr>";
+
+          $("#muscleTable tbody").append(tr_str);
+        }
+      },
+      error: function(xhr, status, error) {
+        console.log(error);
+        alert('An error occurred while retrieving muscle information.');
+      }
+    });
+  });
+
+  $('#addToWorkoutBtn').click(function() {
+    var muscleId = $('#muscleIdInput').val().trim();
+
+    if (muscleId !== '') {
+      var isValidMuscleId = false;
+
+      $('#muscleTable tbody tr').each(function() {
+        var tableMuscleId = $(this).find('td:eq(0)').text();
+
+        if (tableMuscleId === muscleId) {
+          isValidMuscleId = true;
+          return false; // Break the loop
+        }
+      });
+
+      if (isValidMuscleId) {
+        if (muscleList.includes(muscleId)) {
+          alert('Muscle ID already added to the workout list.');
+        } else {
+          if (muscleList.length >= 5) {
+            muscleList.shift();
+          }
+
+          muscleList.push(muscleId);
+
+          updateMuscleList();
+          $('#muscleIdInput').val('');
+        }
+      } else {
+        alert('Invalid Muscle ID. Please enter a valid Muscle ID.');
+      }
+    } else {
+      alert('Please enter a valid Muscle ID.');
+    }
+  });
+
+  $('#addWorkoutBtn').click(function() {
+    // Add your code for the "Add Workout" button functionality here
+    // This code will execute when the button is clicked
+  });
+});
+</script>
+
 <?php
-include_once "Footer.php"
+include_once "Footer.php";
 ?>
